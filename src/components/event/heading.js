1015f490d933ec336f4204b643983460b4c798b4
HAINES-DESKTOP\richa <hello@richardhaines.dev>
2020-12-15 21:37:26 +0100

    highlight instead of circle

--- src/components/event/heading.js
+++ src/components/event/heading.js
@@ -3,7 +3,7 @@ import { Text } from "@chakra-ui/react";
 import { RoughNotation } from "react-rough-notation";
 
 const Heading = ({ children }) => (
-  <RoughNotation type="underline" show={false} color="#e93f79">
+  <RoughNotation type="highlight" show={false} color="#e93f79">
     <Text fontSize="2xl" m={0} textAlign="center" fontFamily="heading">
       {children}
     </Text>
